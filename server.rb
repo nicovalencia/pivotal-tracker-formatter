051acfdea3ac73524fd6593d2b48a60d3391101e
nicovalencia <hello@nico.io>
2012-07-11 18:16:47 -0600

    adding basic auth

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  [username, password] == [ENV['HTTP_USERNAME'], ENV['HTTP_PASSWORD']]
+end
+
 get '/' do
   erb :index
 end
